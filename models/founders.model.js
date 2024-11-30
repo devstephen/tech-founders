5ec41fc065abc752eefbb1f6dddebb1da0b5619b
Fatolu Stephen <smjssp@gmail.com>
2024-11-30 10:41:36 +0100

    feat: create founder schema and model with validation

---
+++ models/founders.model.js
@@ -0,0 +1,37 @@
+import { Schema, Model } from 'mongoose'
+import Joi from 'joi'
+
+const founderSchema = new Schema({
+  id: {
+    type: String,
+    required: true,
+    default: crypto.randomUUID(),
+    unique: true,
+  },
+
+  name: {
+    type: String,
+    required: true,
+  },
+  company: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+})
+
+export const Founder = Model('founder', founderSchema)
+
+export const validateFounder = (founder) => {
+  const schema = Joi.object({
+    id: Joi.string().replace(),
+    name: Joi.string().min(5).max(50).required(),
+    company: Joi.string().min(5).required(),
+    description: Joi.string().min(10).required(),
+  })
+
+  return schema.validate(founder)
+}
